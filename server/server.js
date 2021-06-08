b3fe5cf262ea91853f79bdfce8c5d14b3a31cee5
chiragpatel500 <chirag12354@gmail.com>
2021-06-08 12:01:44 +0200

    Server update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+
+
 app.listen(port, () => {
     console.log("Aerospot Backend")
     console.log('Server is running on ' + port + 'port');
