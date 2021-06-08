da99c1e0f24a2296fc6134510987c388d0e45576
chiragpatel500 <chirag12354@gmail.com>
2021-06-08 12:30:00 +0200

    updates

---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+        "mongodb+srv://chiragpatel500:Playstation3@aerospot-cluster.7bcoz.mongodb.net/flightSpot?retryWrites=true&w=majority",
+};
--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoURI = require("./config.js").mongoURI;const app = express();
+const cors = require("cors");
+
+//initialize express app
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+
+//connect to DB
+mongoose
+    .connect(mongoURI, { useNewurlPraser: true, useCreateIndex: true })
+    .then(() => console.log("Connection to Mongo DB established"))
+    .catch((err) => console.log(err));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-
-
 app.listen(port, () => {
-    console.log("Aerospot Backend")
-    console.log('Server is running on ' + port + 'port');
+  console.log("Aerospot Backend");
+  console.log("Server is running on " + port + "port");
 });
