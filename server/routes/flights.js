bcd5da666356952d0fdd03fe4ca9dd240b741dcd
chiragpatel500 <chirag12354@gmail.com>
2021-06-08 15:20:41 +0200

    UPDATE

--- server/routes/flights.js
+++ server/routes/flights.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const flightModel = require("../models.js/flightModels");
 
 router.get('/all', (req, res) => {
-    res.send({ msg: 'Test route.' });
+    flightModel.find({}, function (err, flightSchema) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(flightSchema);
+        }
+    });
 });
 
 
