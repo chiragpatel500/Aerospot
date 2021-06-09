ea2a8f7feebf5d806ea7ec61bdd5f15359db4603
chiragpatel500 <chirag12354@gmail.com>
2021-06-09 11:18:49 +0200

    context update

---
+++ client/src/Context/flightsContext.js
@@ -0,0 +1,22 @@
+// 1.import the modules
+import React, { createContext, useState, useEffect } from 'react';
+
+
+//2.inititalize the context
+const initFlightsContext = {
+    flights: []
+};
+
+//3. create context
+export const FlightsContext = createContext(initFlightsContext);
+
+//4.make provider =>value/ children 
+export const flightsContextProvider = ({ children }) => {
+    const [flights, setFlights] = useState(initFlightsContext.flights);
+    console.log(flights);
+    return (
+        <CocktailsContext.Provider value={{ flights, setFlights }}>
+            {children}
+        </CocktailsContext.Provider>
+    )
+};
