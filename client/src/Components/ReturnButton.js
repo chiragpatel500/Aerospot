c5c50d5411ab28184112a2294853c1228b3259b5
chiragpatel500 <chirag12354@gmail.com>
2021-06-09 11:23:52 +0200

    update

---
+++ client/src/Components/ReturnButton.js
@@ -0,0 +1,41 @@
+// imports
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from "@material-ui/icons/Restore";
+import history from "../history";
+
+// Css
+const useStyles = makeStyles({
+    root: {
+        backgroundColor: "skyblue",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        position: "fixed",
+        width: "100%",
+        bottom: 0,
+    },
+});
+
+
+// Bottom navigation button to return to previous page
+export default function SimpleBottomNavigation() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    // Return to view the previous page
+    return (
+        <BottomNavigation
+            value={value}
+            onClick={(event, newValue) => {
+                history.goBack();
+            }}
+            showLabels
+            className={classes.root}
+        >
+            <BottomNavigationAction label="Return" icon={<RestoreIcon />} />
+        </BottomNavigation>
+    );
+}
