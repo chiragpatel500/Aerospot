cfb611b7c5c2d1e1e395b16d3b582bf1f6406fb8
chiragpatel500 <chirag12354@gmail.com>
2021-06-28 12:00:18 +0200

    post req update

--- client/src/Components/UploadForm.js
+++ client/src/Components/UploadForm.js
@@ -2,29 +2,45 @@ import React, { useState } from "react";
 import { AuthContext } from "../context/authContext";
 
 const UpLoadForm = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [image, setImage] = useState("");
   const [type, setType] = useState("");
   const [route, setRoute] = useState("");
   const [built, setBuilt] = useState("");
-  const submitForm = () => {};
+  
+  const PostDetails = () => {
+    const data = newFormData();
+    data.append("file", image);
+    data.append("upload_preset", "aerospot");
+    data.append("cloud_name", "chiragpatel500");
+    fetch("	https://api.cloudinary.com/v1_1/chiragpatel500/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
 
   return (
     <div className="UpLoadForm">
-      <form
-        style={
-          ({ marginTop: "30%" },{ display:"grid"})
-        }
-      >
+      <form style={({ marginTop: "30%" }, { display: "grid" })}>
         <input
           type="file"
-          value={selectedFile}
-          onChange={(e) => setSelectedFile(e.target.files[0])}
+          name="image"
+          value={image}
+          onChange={(e) => setImage(e.target.files[0])}
         />
         <label>
           Type:
           <input
             type="text"
             name="type"
+            value={type}
             onChange={(e) => setType(e.target.value)}
           />
         </label>
@@ -33,6 +49,7 @@ const UpLoadForm = () => {
           <input
             type="text"
             name="route"
+            value={route}
             onChange={(e) => setRoute(e.target.value)}
           />
         </label>
@@ -41,10 +58,11 @@ const UpLoadForm = () => {
           <input
             type="text"
             name="built"
+            value={built}
             onChange={(e) => setBuilt(e.target.value)}
           />
         </label>
-        <button onClick={submitForm}>Submit</button>
+        <button onClick={() => PostDetails()}>Submit</button>
       </form>
     </div>
   );
